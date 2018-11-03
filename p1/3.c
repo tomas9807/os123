546e8cc9873807851f91ccecfa290f63b82178b4
tomas9807 <tomas9807@protonmail.com>
2018-11-03 17:39:49 -0400

    ucp fine

--- p1/3.c
+++ p1/3.c
@@ -4,57 +4,69 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 
 
-void print_str(int fd,char const *string){
-    int length_str = (int) strlen(string);
-    for (int i=0;i<=length_str;i++){
-        write(fd,&string[i],sizeof(char));
+unsigned long get_file_size(char const *file_path){
+    struct stat sb;
+    if (stat(file_path,&sb)){
+        perror("no se pudo obtener el tamano del archivo");
+        exit(1);
     }
-    write(STDOUT_FILENO,"\n",sizeof(char));
+    else{
+        return (unsigned long) sb.st_size;
+    }
 }
 
 
 
 int main (int argc, char *argv[]){
-    
+    size_t min_buff_size = 2;
+    size_t max_buff_size = 16383;
+
     if (argc!=4){
 
-    print_str(STDERR_FILENO,"Ha ocurrido un error con los parametros ingresados, la sintaxis es: ucp buffersize file1 file2 \n");
+    perror("Ha ocurrido un error con los parametros ingresados, la sintaxis es: ucp buffersize file1 file2 \n");
     exit(1);
 
     }
   
-    int const BUFFSIZE  =  atoi(argv[1]);
+    size_t  BUFFSIZE  =  (size_t) atoi(argv[1]);
     char const *FILE1 = argv[2];
     char const *FILE2 = argv[3];
     
     if (!BUFFSIZE || !*FILE1 || !*FILE2){
-        print_str(STDERR_FILENO,"Ha ocurrido un error con los parametros ingresados, la sintaxis es: ucp buffersize file1 file2");
+        perror("Ha ocurrido un error con los parametros ingresados, la sintaxis es: ucp buffersize file1 file2 \n");
        
        fflush(stdout);
         exit(1);
     }
    
 
-    if (BUFFSIZE>1 && BUFFSIZE<16384){
-
+    if (BUFFSIZE >= min_buff_size && BUFFSIZE<=max_buff_size){
+            unsigned long file_size;
+            if (BUFFSIZE > (file_size=get_file_size(FILE1))){
+                BUFFSIZE = (size_t) file_size;
+            }
         char  BUFFER [BUFFSIZE];
 
         int fd_file1, fd_file2;
         
-        if ((fd_file1=open(FILE1,O_RDONLY)) == -1){
-            if( (fd_file2=open(FILE2,O_WRONLY|O_CREAT,0777)) ==-1 ){
+        if ((fd_file1=open(FILE1,O_RDONLY))){
+        
+           
+            if( (fd_file2=open(FILE2,O_WRONLY|O_CREAT,0777))){
 
 
              if (read(fd_file1,BUFFER,sizeof(BUFFER)) < 0) {
-                  print_str(STDERR_FILENO,"ha ocurrido un error al leer el archivo a copiar");
+                  perror("ha ocurrido un error al leer el archivo a copiar \n");
                   exit(1);
              }
                 
                 if (write(fd_file2,BUFFER,sizeof(BUFFER)) < 0){
-                    print_str(STDERR_FILENO,"ha ocurrido un error mientras se copiaba el archivo");
+                    perror("ha ocurrido un error mientras se copiaba el archivo \n");
                     exit(1);
                 }
             
@@ -68,7 +80,7 @@ int main (int argc, char *argv[]){
             }
             else{
                 
-            print_str(STDERR_FILENO,strcat("Ha ocurrido un error con el segundo archivo ingresado como ->",FILE2));
+            perror(strcat("Ha ocurrido un error con el segundo archivo ingresado como ->",FILE2));
             exit(1);
 
             }
@@ -78,7 +90,7 @@ int main (int argc, char *argv[]){
         }
         else{
 
-        print_str( STDERR_FILENO,strcat(strcat( "Ha ocurrido un error con el primer archivo ingresado como -> ",FILE1),", Es posible que el archivo no exista o no tiene los permisos para leer") );
+         perror(strcat(strcat( "Ha ocurrido un error con el primer archivo ingresado como -> ",FILE1),", Es posible que el archivo no exista o no tiene los permisos para leer \n") );
         exit(1);
         }
         int close1 = close(fd_file1);
@@ -88,7 +100,7 @@ int main (int argc, char *argv[]){
         
     }
     else{
-        print_str(STDERR_FILENO,"El buffer excede el rango establecido de 1 a 16384 bytes");
+        perror("El buffer excede el rango establecido de 1 a 16384 bytes");
         exit(1);
     }
     return 0;
