d30731ac04b05891514806c6b567a07f39920b20
tomas9807 <tomas9807@protonmail.com>
2018-11-05 17:59:02 -0400

    commit

--- p1/src/6.c
+++ p1/src/6.c
@@ -1,5 +1,5 @@
-#include <stdio.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,112 +8,158 @@
 #include <sys/signal.h>
 #include <sys/times.h>
 
-void terminate(int signum){
-    double dub;
+__uid_t type_uid;
+__uid_t uid1;
+size_t count1;
+__uid_t uid2;
+size_t count2;
+
+void print_time(int signum)
+{
     struct tms t;
-    if ((dub = times(&t)) == -1)
-    perror("times() error");
-   else {
-    printf("process was dubbed %f seconds ago.\n\n",
-           ((double) dub));
-    printf("            utime           stime\n");
-    printf("parent:    %f        %f\n",
-           ((double) t.tms_utime),
-           ((double) t.tms_stime));
-    printf("child:     %f        %f\n",
-           ((double) t.tms_cutime),
-           ((double) t.tms_cstime));
-  }
-    exit(1);
-}
 
-
-int main(int argc,char *argv[]){
-if (argc==6){
-
-
-
-__uid_t uid1 = (__uid_t ) atoi(argv[1]);
-size_t  count1 = (size_t )  atoi(argv[2]);
-__uid_t uid2 =  (__uid_t )  atoi(argv[3]);
-size_t count2 =  (size_t ) atoi(argv[4]);
-size_t time_ = (size_t ) atoi(argv[5]);
-
-
-
-
-
-    if (signal(SIGALRM, terminate)== SIG_ERR){
-    perror("no se pudo crear la senal sigalarm");
-    exit(1);
-    } 
-
-    pid_t pdi_udi1 = fork();
-
-    if (pdi_udi1 > 0 ){ //padre
-      pid_t pdi_udi2 = fork(); 
-
-      if (pdi_udi2==0) { //hijo con udi2
-       if(setuid(uid2) < 0 ){
-           perror("no existe usuario con el valor de udi2 o es root (y se ejecuto sin permisos de root)");
-           exit(1);
-       }
-         for(size_t i=0;i<count2;i++){
-             pid_t pdi_fork2 = fork();
-             if(pdi_fork2==0){
-                 while(1){
-
-                 }
-             }
-             else if(pdi_fork2 <0){
-                 perror("error forking from child udi2");
-             }
-             }
-
-      }else if(pdi_udi2<0){
-
-          perror("error forking child udi2");
-
-      }else{  //continue padre
-            puts("countdown : %zus",)
-            alarm(time_);
-            pause();
-
-      }
-
-    }
-    else if(pdi_udi1==0){ //hijo con udi1
-    if(setuid(uid1) < 0 ){
-           perror("no existe usuario con el valor de udi1  o es root (y se ejecuto sin permisos de root)");
-           exit(1);
-       }
-         for(size_t i=0;i<count1;i++){
-             pid_t pdi_fork1 = fork();
-             if(pdi_fork1==0){
-                 while(1){
-
-                 }
-             }
-             else if(pdi_fork1 <0){
-                 perror("error forking from child udi1");
-             }
-             }
-    }else{
-        perror("error forking child udi1");
+    if ((times(&t)) == -1)
+    {
+        fprintf(stderr, "times() error in %u", type_uid);
+        exit(1);
     }
 
+    else
+    {
+        __uid_t current_uid;
+        size_t current_count;
 
-   
+        if (current_uid == 1)
+        {
+            current_uid = uid1;
+            current_count = count1;
+        }
+        else
+        {
+            current_uid = uid2;
+            current_count = count2;
+        }
 
-    
+        printf("%u  %zu %f %f %f \n", current_uid, current_count, (double)t.tms_utime, (double)t.tms_stime, (double)t.tms_stime + (double)t.tms_utime);
+        fflush(stdout);
+    }
+    killpg(getpgrp(), SIGTERM);
 
-
+    exit(0);
 }
-else{
-    perror("ha ocurrido un error con los parametros, la sintaxis es: ts uid1 count1 uid2 count2 time");
-    exit(1);
-}
 
+int main(int argc, char *argv[])
+{
+    if (argc == 6)
+    {
 
-return 0;
+        uid1 = (__uid_t)atoi(argv[1]);
+        count1 = (size_t)atoi(argv[2]);
+        uid2 = (__uid_t)atoi(argv[3]);
+        count2 = (size_t)atoi(argv[4]);
+        size_t time_ = (size_t)atoi(argv[5]);
+
+        pid_t pdi_udi1 = fork();
+
+        if (pdi_udi1 > 0)
+        { //padre
+            pid_t pdi_udi2 = fork();
+
+            if (pdi_udi2 == 0)
+            { //hijo con udi2
+
+                if (setuid(uid2) < 0)
+                {
+                    perror("no existe usuario con el valor de udi2 o es root (y se ejecuto sin permisos de root)");
+                    exit(1);
+                }
+
+                for (size_t i = 0; i < count2; i++)
+                {
+                    pid_t pdi_fork2 = fork();
+                    if (pdi_fork2 == 0)
+                    {
+
+                        while (1)
+                        {
+                        }
+                    }
+
+                    else if (pdi_fork2 < 0)
+                    {
+                        perror("error forking from child udi2");
+                    }
+                }
+                wait(NULL);
+
+                type_uid = 2;
+                if (signal(SIGUSR1, print_time) == SIG_ERR)
+                {
+                    perror("no se pdo crear la senal sigalarm");
+                    exit(1);
+                }
+
+                wait(NULL);
+            }
+            else if (pdi_udi2 < 0)
+            {
+
+                perror("error forking udi2");
+            }
+            else
+            { //continue padre
+                puts("  UID  COUNT  USERTIME   SYSTEMTIME   USER+SYSTEM \n\n");
+                alarm(time_);
+                pause();
+
+                killpg(getpgrp(), SIGUSR1);
+            }
+        }
+        else if (pdi_udi1 == 0)
+        { //hijo con udi1
+
+            if (setuid(uid1) < 0)
+            {
+                perror("no existe usuario con el valor de udi1  o es root (y se ejecuto sin permisos de root)");
+                exit(1);
+            }
+
+            for (size_t i = 0; i < count1; i++)
+            {
+                pid_t pdi_fork1 = fork();
+                if (pdi_fork1 == 0)
+                {
+
+                    while (1)
+                    {
+                    }
+                }
+
+                else if (pdi_fork1 < 0)
+                {
+                    perror("error forking from child udi2");
+                }
+            }
+
+            type_uid = 1;
+            if (signal(SIGUSR1, print_time) == SIG_ERR)
+            {
+                perror("no se pudo crear la senal sigalarm");
+                exit(1);
+            }
+            wait(NULL);
+        }
+
+        else
+        {
+            perror("error forking child udi1");
+        }
+    }
+    else
+    {
+        perror("ha ocurrido un error con los parametros, la sintaxis es: ts uid1 count1 uid2 count2 time");
+        exit(1);
+    }
+
+    return 0;
 }
