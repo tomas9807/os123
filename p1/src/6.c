2d91c3b26df557eca33ce241dcfc97645543a120
tomas9807 <tomas9807@protonmail.com>
2018-11-06 14:04:47 -0400

    commit

--- p1/src/6.c
+++ p1/src/6.c
@@ -162,13 +162,14 @@ int main(int argc, char *argv[])
                 perror("error forking uid2");
                 exit(1);
             }
-            puts("  UID  COUNT  USERTIME   SYSTEMTIME   USER+SYSTEM \n");
+            printf("espere %zus \n\n",time_);
             sleep(time_);
-
+            puts("  UID  COUNT  USERTIME   SYSTEMTIME   USER+SYSTEM \n");
             kill(ppid_udi1, SIGUSR1);
             kill(ppid_udi2, SIGUSR1);
             waitpid(ppid_udi1, NULL, 0);
             waitpid(ppid_udi2, NULL, 0);
+         
         }
         else
         {
