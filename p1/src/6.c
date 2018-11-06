6ebf7a3b07340c194ddffcf3d0a737564098f31f
tomas9807 <tomas9807@protonmail.com>
2018-11-05 22:23:48 -0400

    commit

--- p1/src/6.c
+++ p1/src/6.c
@@ -7,45 +7,15 @@
 #include <signal.h>
 #include <sys/signal.h>
 #include <sys/times.h>
+size_t current_count;
+size_t *pids_children;
 
-__uid_t type_uid;
-__uid_t uid1;
-size_t count1;
-__uid_t uid2;
-size_t count2;
-
-void print_time(int signum)
+void kill_children(int signum)
 {
-    struct tms t;
-
-    if ((times(&t)) == -1)
+    for (size_t i = 0; i < current_count; i++)
     {
-        fprintf(stderr, "times() error in %u", type_uid);
-        exit(1);
+        kill(pids_children[i], SIGTERM); //wait for the 5 children and gather time
     }
-
-    else
-    {
-        __uid_t current_uid;
-        size_t current_count;
-
-        if (current_uid == 1)
-        {
-            current_uid = uid1;
-            current_count = count1;
-        }
-        else
-        {
-            current_uid = uid2;
-            current_count = count2;
-        }
-
-        printf("%u  %zu %f %f %f \n", current_uid, current_count, (double)t.tms_utime, (double)t.tms_stime, (double)t.tms_stime + (double)t.tms_utime);
-        fflush(stdout);
-    }
-    killpg(getpgrp(), SIGTERM);
-
-    exit(0);
 }
 
 int main(int argc, char *argv[])
@@ -53,80 +23,27 @@ int main(int argc, char *argv[])
     if (argc == 6)
     {
 
+        int tics_per_second = sysconf(_SC_CLK_TCK);
+        __uid_t uid1;
+        size_t count1;
+        __uid_t uid2;
+        size_t count2;
+
         uid1 = (__uid_t)atoi(argv[1]);
         count1 = (size_t)atoi(argv[2]);
         uid2 = (__uid_t)atoi(argv[3]);
         count2 = (size_t)atoi(argv[4]);
         size_t time_ = (size_t)atoi(argv[5]);
 
-        pid_t pdi_udi1 = fork();
-
-        if (pdi_udi1 > 0)
-        { //padre
-            pid_t pdi_udi2 = fork();
-
-            if (pdi_udi2 == 0)
-            { //hijo con udi2
-
-                if (setuid(uid2) < 0)
-                {
-                    perror("no existe usuario con el valor de udi2 o es root (y se ejecuto sin permisos de root)");
-                    exit(1);
-                }
-                pid_t pdi_fork2;
-                for (size_t i = 0; i < count2; i++)
-                {
-                    pdi_fork2 = fork();
-                    if (pdi_fork2 == 0)
-                    {
-                        while (1)
-                        {
-                        }
-                    }
-
-                    else if (pdi_fork2 < 0)
-                    {
-                        perror("error forking from child udi2");
-                    }
-                }
-               
-
-                type_uid = 2;
-                if (signal(SIGUSR1, print_time) == SIG_ERR)
-                {
-                    perror("no se pdo crear la senal sigalarm");
-                    exit(1);
-                }
-
-                wait(NULL);
-            }
-            else if (pdi_udi2 < 0)
+        pid_t ppid_udi1 = fork();
+        if (ppid_udi1 == 0)
+        {
+            pids_children = malloc(sizeof(int) * count1);
+            current_count = count1;
+            for (size_t i = 0; i < count1; i++) //fork 5 children(or 5 grandchildren) from  the first child and set them in an endless loop
             {
-
-                perror("error forking udi2");
-            }
-            else
-            { //continue padre
-                puts("  UID  COUNT  USERTIME   SYSTEMTIME   USER+SYSTEM \n\n");
-                alarm(time_);
-                pause();
-
-                killpg(getpgrp(), SIGUSR1);
-            }
-        }
-        else if (pdi_udi1 == 0)
-        { //hijo con udi1
-
-            if (setuid(uid1) < 0)
-            {
-                perror("no existe usuario con el valor de udi1  o es root (y se ejecuto sin permisos de root)");
-                exit(1);
-            }
-
-            for (size_t i = 0; i < count1; i++)
-            {
-                pid_t pdi_fork1 = fork();
-                if (pdi_fork1 == 0)
+                pid_t pid = fork();
+                if (pid == 0)
                 {
 
                     while (1)
@@ -134,31 +51,129 @@ int main(int argc, char *argv[])
                     }
                 }
 
-                else if (pdi_fork1 < 0)
+                else if (pid < 0)
                 {
-                    perror("error forking from child udi2");
+                    perror("error forking from child ");
+                }
+                else
+                {
+                    pids_children[i] = pid;
                 }
             }
 
-            type_uid = 1;
-            if (signal(SIGUSR1, print_time) == SIG_ERR)
+            if (signal(SIGUSR1, kill_children) == SIG_ERR) //set signal to the first child  so it print the time gathered by wait()
             {
-                perror("no se pudo crear la senal sigalarm");
+                perror("cannot create signal");
                 exit(1);
             }
-            wait(NULL);
+
+            for (size_t i = 0; i < count1; i++)
+            {
+                waitpid(pids_children[i], NULL, 0); //wait for the 5 children and gather time
+            }
+            struct tms t;
+            
+            if ((times(&t)) == -1)
+            {
+                perror("error times");
+                exit(1);
+            }
+
+            else
+            {
+                double usertime = (double)t.tms_cutime / tics_per_second;
+                double systime = (double)t.tms_cstime / tics_per_second;
+                double sumtime = usertime + systime;
+                printf("  %u   %zu    %f    %f    %f \n", uid1, count1, usertime, systime, sumtime);
+                exit(0);
+            }
         }
 
+        else if (ppid_udi1 > 0)
+        { //parent
+
+            pid_t ppid_udi2 = fork();
+            if (ppid_udi2 == 0)
+            {
+                pids_children = malloc(sizeof(int) * count2);
+                current_count = count2;
+                for (size_t i = 0; i < count2; i++) //fork 5 children(or 5 grandchildren) from  the first child and set them in an endless loop
+                {
+                    pid_t pid = fork();
+                    if (pid == 0)
+                    {
+
+                        while (1)
+                        {
+                        }
+                    }
+
+                    else if (pid < 0)
+                    {
+                        perror("error forking from child ");
+                    }
+                    else
+                    {
+                        pids_children[i] = pid;
+                    }
+                }
+
+                if (signal(SIGUSR1, kill_children) == SIG_ERR) //set signal to the first child  so it print the time gathered by wait()
+                {
+                    perror("cannot create signal");
+                    exit(1);
+                }
+
+                for (size_t i = 0; i < count2; i++)
+                {
+                    waitpid(pids_children[i], NULL, 0); //wait for the 5 children and gather time
+                }
+                struct tms t;
+              
+                if ((times(&t)) == -1)
+                {
+                    perror("error times");
+                    exit(1);
+                }
+
+                else
+                {
+
+                    double usertime = (double)t.tms_cutime / tics_per_second;
+                    double systime = (double)t.tms_cstime / tics_per_second;
+                    double sumtime = usertime + systime;
+                    printf("  %u   %zu    %f    %f    %f \n", uid2, count2, usertime, systime, sumtime);
+
+                    exit(0);
+                }
+            }
+            else if(ppid_udi2 < 0 )
+            {
+                perror("error forking uid2");
+                exit(1);
+            }
+            puts("  UID  COUNT  USERTIME   SYSTEMTIME   USER+SYSTEM \n");
+            sleep(time_);
+            
+
+            
+            kill(ppid_udi1, SIGUSR1);
+            kill(ppid_udi2, SIGUSR1);
+            waitpid(ppid_udi1, NULL, 0);
+            waitpid(ppid_udi2, NULL, 0);
+        }
         else
         {
-            perror("error forking child udi1");
+
+            perror("error forking child uid1");
+            exit(1);
         }
+
+        return 0;
     }
     else
     {
-        perror("ha ocurrido un error con los parametros, la sintaxis es: ts uid1 count1 uid2 count2 time");
+        perror("no hay suficientes parametros la sintaxis es: ts uid1 count1 uid2 count2 time");
         exit(1);
     }
-
-    return 0;
 }
